39084ffc636efd11132258fca284045e5951da03
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2016-10-01 10:32:40 +0800

    initial

--- phpecm2cue.c
+++ phpecm2cue.c
@@ -5,33 +5,33 @@
 #include "php.h"
 #include "php_ecm2cue.h"
 
-static function_entry hello_functions[] = {
-    PHP_FE(hello_world, NULL)
+static function_entry ecm2cue_functions[] = {
+    PHP_FE(ecm2cue, NULL)
     {NULL, NULL, NULL}
 };
 
-zend_module_entry hello_module_entry = {
+zend_module_entry ecm2cue_module_entry = {
 #if ZEND_MODULE_API_NO >= 20010901
     STANDARD_MODULE_HEADER,
 #endif
-    PHP_HELLO_WORLD_EXTNAME,
-    hello_functions,
+    PHP_ECM2CUE_EXTNAME,
+    ecm2cue_functions,
     NULL,
     NULL,
     NULL,
     NULL,
     NULL,
 #if ZEND_MODULE_API_NO >= 20010901
-    PHP_HELLO_WORLD_VERSION,
+    PHP_ECM2CUE_VERSION,
 #endif
     STANDARD_MODULE_PROPERTIES
 };
 
 #ifdef COMPILE_DL_HELLO
-ZEND_GET_MODULE(hello)
+ZEND_GET_MODULE(ecm2cue)
 #endif
 
-PHP_FUNCTION(hello_world)
+PHP_FUNCTION(ecm2cue)
 {
     RETURN_STRING("Hello World", 1);
 }
